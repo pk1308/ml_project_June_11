e409c71bdabf188c25a05785f16424a929471805
PK <princvkurien@gmail.com>
2022-07-14 01:34:59 +0400

    loguru

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,4 +1,4 @@
-import logging
+from loguru import logger as logging
 from datetime import datetime
 import os
 import pandas as pd
@@ -16,11 +16,7 @@ LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 
 
-logging.basicConfig(filename=LOG_FILE_PATH,
-filemode="w",
-format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
-level=logging.INFO
-)
+logging.add(LOG_FILE_PATH,format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {line:<4} | {file} | {function} |{message}",level="DEBUG")
 
 def get_log_dataframe(file_path):
     data=[]
