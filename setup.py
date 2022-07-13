376df1efaa13a76285d5815f232ac525329c3ce8
PK <princvkurien@gmail.com>
2022-07-13 22:08:16 +0400

    u

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.7"
+VERSION="0.0.1"
 AUTHOR="PK"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
